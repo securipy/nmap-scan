275b08cd445576f2e106db8aee8114f2769d06c9
goldrak <goldrak@gmail.com>
2015-05-21 21:41:07 +0200

    Comienzo reescritura

---
+++ model/nmap_scan.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+#-*-coding:utf-8-*-
+
+import nmap
+
+__author__ 		= "GoldraK & Roger Serentill"
+__credits__ 	= "GoldraK & Roger Serentill"
+__version__ 	= "0.1"
+__maintainer__ 	= "GoldraK & Roger Serentill"
+__email__ 		= "goldrak@gmail.com, hello@rogerserentill.com"
+__status__ 		= "Development"
+
+
+
+class nmap-scan:
+
+	def __init__(self, host=127.0.0.1):
+		self.host = host
+
+
+	def scan_discover(self, arguments = None):
+		nm = nmap.PortScanner()
+		if arguments == None:
+			nm.scan(hosts=host_scan, arguments=arguments)
+		else:
+			nm.scan(hosts=host_scan, arguments='-n -sP -PE -PA21,23,80,3389')
+		hosts_list = [(x, nm[x]['status']['state']) for x in nm.all_hosts()]
+		return hosts_list
+
+	def scan(self,arguments = None):
+		nm = nmap.PortScanner()
+		if arguments == None:
+			nm.scan(hosts=host_scan)
+		else:
+			nm.scan(hosts=host_scan, arguments=arguments)
+		hosts_list = [(x, nm[x]['status']['state']) for x in nm.all_hosts()]
+		return hosts_list
